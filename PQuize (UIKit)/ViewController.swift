73708204aa3115f56d1cbd9064c0e5430b92337a
Fedor Boretsky <fedor.boretsky@gmail.com>
2022-10-23 14:17:47 +0300

    Initial Commit

---
+++ PQuize (UIKit)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PQuize (UIKit)
+//
+//  Created by Fedor Boretsky on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
